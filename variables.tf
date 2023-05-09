a1b42aa1a41a4999bd342f80b9aab566a297030f
Veaceslav Bivol <vbivol88@gmail.com>
2023-05-08 22:33:40 -0400

    use naming and variables

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr_block" {
+
+}
+
+variable "public_subnet_cidr" {
+
+}
+
+variable "private_subnet_cidr" {
+
+}
+
+variable "env_prefix" {
+
+}
+
+variable "availability_zones" {
+
+}
+
