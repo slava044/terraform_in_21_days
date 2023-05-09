3ef55e37e28d7c91502bf320671623122d0f52ee
Veaceslav Bivol <vbivol88@gmail.com>
2023-05-09 15:11:04 -0400

    create instances in public&private subnet

---
+++ instance.tf
@@ -0,0 +1,76 @@
+resource "aws_instance" "public" {
+  ami                         = "ami-0889a44b331db0194"
+  instance_type               = "t3.micro"
+  key_name                    = "class"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.public[0].id
+ 
+  tags = {
+    Name = "${var.env_prefix}-public"
+  }
+}
+
+resource "aws_security_group" "public" {
+  name        = "${var.env_prefix}-public"
+  description = "Allow inbound traffic from my ip"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh from my ip"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["108.216.6.65/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-public"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                         = "ami-0889a44b331db0194"
+  instance_type               = "t3.micro"
+  key_name                    = "class"
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  subnet_id                   = aws_subnet.private[0].id
+ 
+  tags = {
+    Name = "${var.env_prefix}-private"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env_prefix}-private"
+  description = "Allow ssh inbound from vpc"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh from Vpc cidr range"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-private"
+  }
+}
