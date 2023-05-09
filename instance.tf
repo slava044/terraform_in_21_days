a67a06a65e225bf075cb9f3657b5025bededc912
Veaceslav Bivol <vbivol88@gmail.com>
2023-05-09 18:19:29 -0400

    use data source for ami

--- instance.tf
+++ instance.tf
@@ -1,11 +1,32 @@
+data "aws_ami" "amazonLinux" {
+  owners      = ["amazon"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
 resource "aws_instance" "public" {
-  ami                         = "ami-0889a44b331db0194"
+  ami                         = data.aws_ami.amazonLinux.id
   instance_type               = "t3.micro"
   key_name                    = "class"
   vpc_security_group_ids      = [aws_security_group.public.id]
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public[0].id
- 
+
   tags = {
     Name = "${var.env_prefix}-public"
   }
@@ -17,11 +38,11 @@ resource "aws_security_group" "public" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "ssh from my ip"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["108.216.6.65/32"]
+    description = "ssh from my ip"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["108.216.6.65/32"]
   }
 
   egress {
@@ -38,12 +59,12 @@ resource "aws_security_group" "public" {
 }
 
 resource "aws_instance" "private" {
-  ami                         = "ami-0889a44b331db0194"
-  instance_type               = "t3.micro"
-  key_name                    = "class"
-  vpc_security_group_ids      = [aws_security_group.public.id]
-  subnet_id                   = aws_subnet.private[0].id
- 
+  ami                    = data.aws_ami.amazonLinux.id
+  instance_type          = "t3.micro"
+  key_name               = "class"
+  vpc_security_group_ids = [aws_security_group.public.id]
+  subnet_id              = aws_subnet.private[0].id
+
   tags = {
     Name = "${var.env_prefix}-private"
   }
@@ -55,11 +76,11 @@ resource "aws_security_group" "private" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description      = "ssh from Vpc cidr range"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [var.vpc_cidr_block]
+    description = "ssh from Vpc cidr range"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
   }
 
   egress {
