ca6ec2843ac7fc92ad8c4fa8cd21ba94574b57fa
Veaceslav Bivol <vbivol88@gmail.com>
2023-06-07 13:42:11 -0400

    Create & validate certificate

--- modules/lb/main.tf
+++ modules/lb/main.tf
@@ -16,10 +16,10 @@ resource "aws_security_group" "load_balancer" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description = "http to lb"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    description = "https to ELB"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -28,7 +28,6 @@ resource "aws_security_group" "load_balancer" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -56,8 +55,10 @@ resource "aws_lb_target_group" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = 443
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.main.arn
+  ssl_policy        = "ELBSecurityPolicy-2016-08" 
 
   default_action {
     type             = "forward"
@@ -73,6 +74,38 @@ resource "aws_route53_record" "www" {
   zone_id  = data.aws_route53_zone.main.zone_id
   name     = "www.${data.aws_route53_zone.main.name}"
   type     = "CNAME"
-  ttl      = "300"
+  ttl      = 300
   records  = [aws_lb.main.dns_name]
+}
+
+resource "aws_acm_certificate" "main" {
+  domain_name       = "www.${data.aws_route53_zone.main.name}"
+  validation_method = "DNS"
+
+   tags = {
+    Name = var.env_prefix
+   }
+}
+
+resource "aws_route53_record" "domain_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.domain_validation : record.fqdn]
 }
