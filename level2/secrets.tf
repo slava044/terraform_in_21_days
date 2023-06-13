0c389daded951467d290b550c0472d7c18656a01
Veaceslav Bivol <vbivol88@gmail.com>
2023-06-13 05:57:22 -0400

    create secret for database

---
+++ level2/secrets.tf
@@ -0,0 +1,12 @@
+data "aws_secretsmanager_secret_version" "main" {
+  secret_id = data.aws_secretsmanager_secret.rds_password.id
+}
+
+data "aws_secretsmanager_secret" "rds_password" {
+  name = "main/rds/password"
+
+}
+
+locals {
+  rds_password = jsondecode(data.aws_secretsmanager_secret_version.main.secret_string)["password"]
+}
